dc0d1d83d95dd6f5e841081b117ced9a86ceac03
trueberryless <trueberryless+github@gmail.com>
2024-05-07 21:57:08 +0200

    Initial setup

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightBlog from "starlight-blog";
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        starlight({
+            title: "Blog",
+            social: {
+                github: "https://github.com/trueberryless-org/blog",
+            },
+            logo: {
+                light: "./src/assets/light-logo.png",
+                dark: "./src/assets/dark-logo.png",
+                replacesTitle: true,
+            },
+            plugins: [
+                starlightBlog({
+                    title: "Deep Thoughts",
+                    authors: {
+                        trueberryless: {
+                            name: "trueberryless",
+                            title: "Felix Schneider",
+                            picture: "/trueberryless.png", // Images in the `public` directory are supported.
+                            url: "https://trueberryless.org",
+                        },
+                    },
+                }),
+            ],
+            customCss: ["./src/styles/custom.css"],
+            pagination: false,
+        }),
+    ],
+    redirects: {
+        "/": "/blog",
+    },
+});
